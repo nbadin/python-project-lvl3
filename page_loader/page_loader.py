164110639b680d2c82c0455f899c104bf0c6231f
nbadin <nbadin011@gmail.com>
2022-06-30 00:54:45 +0300

    step 1

--- page_loader/page_loader.py
+++ page_loader/page_loader.py
@@ -1,22 +1,30 @@
-from bs4 import BeautifulSoup
 import requests
+import re
 import os
-from page_loader.src.get_names import get_basic_name
-from page_loader.src.images import dowload_image
+from bs4 import BeautifulSoup
 
 
 def download(url, path=os.getcwd()):
-    dir_path_for_files = f'{get_basic_name(url)}_files'
-    os.makedirs(dir_path_for_files)
-    filepath = f'{get_basic_name(url)}.html'
     response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    images = soup.find_all('img')
-    for image in images:
-        image_path = dowload_image(image['src'], dir_path_for_files)
-        image['src'] = image_path
 
-    with open(filepath, 'w') as f:
-        f.write(soup.prettify())
+    if response.ok:
+        filename = make_name(url)
 
-    return filepath
+        soup = BeautifulSoup(response.text, 'html.parser')
+        filepath = f'{path}/{filename}'
+        with open(filepath, 'w') as f:
+            f.write(soup.prettify())
+
+        return filepath
+
+    else:
+        raise ValueError(
+            f'Page wasn\'t downloaded! Response code: {response.status_code}'
+        )
+
+
+def make_name(url):
+    name_without_scheme = re.sub(r'^(http://|https://)', '', url)
+    name_without_ext = re.sub(r'\.html', '', name_without_scheme)
+
+    return '-'.join(re.split(r'\W', name_without_ext)) + '.html'
--- tests/test_download.py
+++ tests/test_download.py
@@ -1,14 +1,21 @@
 from page_loader.page_loader import download
 import tempfile
 import os
+import pytest
 
 
+@pytest.fixture
+def downloaded_page():
+    with open('tests/fixture/page.html', 'r') as f:
+        text = f.read()
+        return text
 
-def test_download(requests_mock):
-    requests_mock.get('http://test.com', text='data')
+
+def test_download(requests_mock, downloaded_page):
+    requests_mock.get('http://test.com', text=downloaded_page)
     tmp_path = tempfile.mkdtemp()
     path_to_file = download('http://test.com', tmp_path)
     assert os.path.isfile(path_to_file)
     with open(path_to_file) as f:
         text = f.read()
-        assert text == 'data'
+        assert text == downloaded_page
